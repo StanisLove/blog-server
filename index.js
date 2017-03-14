ca118c4a307ad48304a45e8cb13f0780b56a7621
Stanislav Kravchenko <stankra@yandex.ru>
2017-03-14 23:38:00 +0300

    Add POST methods for postLiked

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var application = express();
 
 var cors = require('cors');
 
+var _ = require('lodash');
+
 var items = require('./data').items;
 var about = require('./data').about;
 
@@ -12,8 +14,25 @@ application.get('/', function(req, res) {
   res.json(items);
 });
 
+application.post('/', function(req, res) {
+  const id = req.query['id']
+  const posts = items.map(function(obj) {
+    if (obj.id == id) {
+      obj.meta.likeCount = _.get(obj, 'meta.likeCount', 0) + 1;
+    }
+    return obj;
+  });
+  res.json(posts)
+});
+
 application.get('/posts/:postId', function(req, res) {
   res.json(items.filter((obj) => obj.id == req.params.postId)[0]);
+});
+
+application.post('/posts/:postId', function(req, res) {
+  const post = items.filter((obj) => obj.id == req.params.postId)[0];
+  post.meta.likeCount = _.get(post, 'meta.likeCount', 0) + 1;
+  res.json(post)
 });
 
 application.get('/about', function(req, res) {
